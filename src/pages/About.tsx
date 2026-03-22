36dbba902e30cbae341381a501a2b5430aaa1d66
MusawarQaderi <musawer00@gmail.com>
2026-03-22 09:56:44 +0100

    Fix syntax error in About.tsx sections

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -191,7 +191,7 @@ export const About = () => {
       </section>
 
       {/* Timeline Section */}
-      <section className="py-24 bg-bg-surface/30">
+      <section className="py-24 bg-bg-surface/30">;
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <motion.div
             initial={{ opacity: 0, y: 30 }}
@@ -251,7 +251,7 @@ export const About = () => {
       </section>
 
       {/* Philosophy Section */}
-      <section className="py-24">
+      <section className="py-24">;
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <motion.div
             initial={{ opacity: 0, y: 30 }}
