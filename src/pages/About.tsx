2d5459ed82687ef38c0bfd63d46c1f265cad3170
Musawar Qaderi <musawer00@gmail.com>
2026-03-30 12:52:55 +0200

    Fix duplicate key in About.tsx
    Removed duplicate 'description' key in About.tsx.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,7 +10,6 @@ export const About = () => {
       title: 'Umschulung - Fachinformatiker für Systemintegration',
       company: 'CBM Projektmanagement GmbH',
       description:
-        description:
 'Absolvierung einer Umschulung zum Fachinformatiker für Systemintegration. Parallel eigenständiger Aufbau von Kenntnissen im Bereich Cloud und DevOps mit Fokus auf Automatisierung und Deployment. Arbeit mit Python, AWS und Docker sowie Beschäftigung mit CI/CD-Prozessen und Linux. Stetige Vertiefung durch praktische Übungen und eigene Projekte.',
       icon: Cloud,},
     {
